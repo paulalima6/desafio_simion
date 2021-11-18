6cfeff523336d76507550b5088de192462bbd53d
ana.slima6@gmail.com <ana.slima6@gmail.com>
2021-11-18 00:24:23 -0300

    first commit

---
+++ src/main/java/br/com/pcon/api/DesafioSimionApplication.java
@@ -0,0 +1,13 @@
+package br.com.pcon.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSimionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSimionApplication.class, args);
+	}
+
+}
